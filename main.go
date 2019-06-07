99de10ce0262150d29242033492396b649686737
Sergey Novikov <snov@snov.me>
2019-06-07 13:38:20 +0200

    Use LookPath (#8)

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/s12v/exec-with-secrets/provider/awsssm"
 	_ "github.com/s12v/exec-with-secrets/provider/azurekeyvault"
 	"os"
+	"os/exec"
 	"syscall"
 )
 
@@ -15,6 +16,7 @@ func main() {
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("exec-with-secrets:", r)
+			os.Exit(1)
 		}
 	}()
 
@@ -23,9 +25,18 @@ func main() {
 		os.Exit(0)
 	}
 
+	path := lookPath(os.Args[1])
 	env := provider.Populate(os.Environ())
-	_ = syscall.Exec(os.Args[1], os.Args[1:], env)
+	_ = syscall.Exec(path, os.Args[1:], env)
 
-	fmt.Println("exec-with-secrets program: Unable to start", os.Args[1])
-	os.Exit(1)
+	panic("Unable to start " + path)
+}
+
+func lookPath(name string) string {
+	path, err := exec.LookPath(name)
+	if err != nil {
+		panic(err)
+	}
+
+	return path
 }
